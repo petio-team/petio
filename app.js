9ff18bc83e9cf205165542cddcfc24fcbb2d69fe
AshDyson <ashley@hypergram.co>
2020-11-29 19:06:34 +0000

    code tidy up

--- app.js
+++ app.js
@@ -2,48 +2,8 @@ const express = require('express');
 const path = require('path');
 const admin = express();
 const frontend = express();
-const open = require('open');
-const { exec } = require('child_process');
-
-// function installPackages() {
-// 	console.log('Installing Packages: API');
-// 	return new Promise(async (resolve) => {
-// 		exec('npm install --prefix ./api/', (err, stout, sterr) => {
-// 			console.log('Installing Packages: API .... Complete');
-// 			resolve(err ? stout : sterr);
-// 		});
-// 	});
-// }
 
 init();
-
-async function start() {
-	// await installPackages();
-	// const api = exec(
-	// 	'npm start --prefix ./api/',
-	// 	function (err, stdout, stderr) {
-	// 		if (err) {
-	// 			throw err;
-	// 		}
-	// 	}
-	// );
-
-	api.stdout.on('data', function (data) {
-		console.log('API Log: ' + data.toString());
-	});
-
-	api.stderr.on('data', function (data) {
-		console.log('API Err: ' + data.toString());
-	});
-
-	api.on('exit', function (code) {
-		console.log('API exited with code ' + code.toString());
-	});
-
-	setTimeout(() => {
-		init();
-	}, 2000);
-}
 
 function init() {
 	let project_folder = __dirname;
