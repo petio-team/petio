296b64b14dfe24adbdc0030cc365ca9e1b79b2a2
AshDyson <ashley@hypergram.co>
2020-12-21 22:26:10 +0000

    Fix person bug

--- api/routes/mail.js
+++ api/routes/mail.js
@@ -1,106 +1,107 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const fs = require('fs');
-const path = require('path');
-const Mailer = require('../mail/mailer');
+const fs = require("fs");
+const path = require("path");
+const Mailer = require("../mail/mailer");
 
-router.post('/create', async (req, res) => {
-	let email = req.body.email;
+router.post("/create", async (req, res) => {
+  let email = req.body.email;
 
-	if (!email) {
-		res.status(500).send('Missing Fields');
-		console.log('Create email config failed');
-		return;
-	}
-	console.log('Creating email config');
-	let configData = {
-		emailEnabled: email.enabled,
-		emailUser: email.user,
-		emailPass: email.pass,
-		emailServer: email.server,
-		emailPort: email.port,
-		emailSecure: email.secure,
-	};
-	configData = JSON.stringify(configData);
-	let config = await createConfig(configData);
-	if (config) {
-		res.json({ config: config });
-	}
+  if (!email) {
+    res.status(500).send("Missing Fields");
+    console.log("Create email config failed");
+    return;
+  }
+  console.log("Creating email config");
+  let configData = {
+    emailEnabled: email.enabled,
+    emailUser: email.user,
+    emailPass: email.pass,
+    emailServer: email.server,
+    emailPort: email.port,
+    emailSecure: email.secure,
+  };
+  configData = JSON.stringify(configData);
+  let config = await createConfig(configData);
+  if (config) {
+    res.json({ config: config });
+  }
 });
 
-router.get('/config', async (req, res) => {
-	let config = await getConfig();
-	let data = false;
-	if (!config) {
-		data = {
-			emailEnabled: false,
-			emailUser: '',
-			emailPass: '',
-			emailServer: '',
-			emailPort: '',
-			emailSecure: false,
-		};
-	} else {
-		data = {
-			emailEnabled: config.emailEnabled,
-			emailUser: config.emailUser,
-			emailPass: config.emailPass,
-			emailServer: config.emailServer,
-			emailPort: config.emailPort,
-			emailSecure: config.emailSecure,
-		};
-	}
-	res.json({
-		config: data,
-	});
+router.get("/config", async (req, res) => {
+  let config = await getConfig();
+  console.log(config);
+  let data = false;
+  if (!config) {
+    data = {
+      emailEnabled: false,
+      emailUser: "",
+      emailPass: "",
+      emailServer: "",
+      emailPort: "",
+      emailSecure: false,
+    };
+  } else {
+    data = {
+      emailEnabled: config.emailEnabled,
+      emailUser: config.emailUser,
+      emailPass: config.emailPass,
+      emailServer: config.emailServer,
+      emailPort: config.emailPort,
+      emailSecure: config.emailSecure,
+    };
+  }
+  res.json({
+    config: data,
+  });
 });
 
-router.get('/test', async (req, res) => {
-	let test = await new Mailer().test();
-	res.json({ result: test.result, error: test.error });
+router.get("/test", async (req, res) => {
+  let test = await new Mailer().test();
+  res.json({ result: test.result, error: test.error });
 });
 
 function getConfig() {
-	return new Promise((resolve, reject) => {
-		let project_folder, configFile;
-		if (process.pkg) {
-			project_folder = path.dirname(process.execPath);
-			configFile = path.join(project_folder, './config/email.json');
-		} else {
-			project_folder = __dirname;
-			configFile = path.join(project_folder, '../config/email.json');
-		}
+  return new Promise((resolve, reject) => {
+    let project_folder, configFile;
+    if (process.pkg) {
+      project_folder = path.dirname(process.execPath);
+      configFile = path.join(project_folder, "./config/email.json");
+    } else {
+      project_folder = __dirname;
+      configFile = path.join(project_folder, "../config/email.json");
+    }
 
-		fs.readFile(configFile, 'utf8', (err, data) => {
-			if (err) {
-				console.log(err);
-				resolve(false);
-				return;
-			}
-			resolve(JSON.parse(data));
-		});
-	});
+    fs.readFile(configFile, "utf8", (err, data) => {
+      if (err) {
+        console.log(err);
+        resolve(false);
+        return;
+      }
+      resolve(JSON.parse(data));
+    });
+  });
 }
 
 function createConfig(data) {
-	return new Promise((resolve, reject) => {
-		let project_folder, configFile;
-		if (process.pkg) {
-			project_folder = path.dirname(process.execPath);
-			configFile = path.join(project_folder, './config/email.json');
-		} else {
-			project_folder = __dirname;
-			configFile = path.join(project_folder, '../config/email.json');
-		}
+  return new Promise((resolve, reject) => {
+    let project_folder, configFile;
+    if (process.pkg) {
+      project_folder = path.dirname(process.execPath);
+      configFile = path.join(project_folder, "./config/email.json");
+    } else {
+      project_folder = __dirname;
+      configFile = path.join(project_folder, "../config/email.json");
+    }
 
-		fs.writeFile(configFile, data, (err) => {
-			if (err) {
-				console.log(err);
-				resolve(false);
-			}
-			resolve(true);
-		});
-	});
+    fs.writeFile(configFile, data, (err) => {
+      if (err) {
+        console.log(err);
+        resolve(false);
+      }
+      resolve(true);
+    });
+  });
 }
 
 module.exports = router;
--- frontend/src/components/PersonCard.js
+++ frontend/src/components/PersonCard.js
@@ -1,51 +1,44 @@
-import React from 'react';
-import { withRouter, Link } from 'react-router-dom';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import 'react-lazy-load-image-component/src/effects/blur.css';
+import React from "react";
+import { withRouter, Link } from "react-router-dom";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 class PersonCard extends React.Component {
-	render() {
-		let knownFor = null;
-		let person = this.props.person;
-		if (!person) {
-			return null;
-		}
-		if (person.known_for) {
-			person.known_for.map((item, key) => {
-				return <Link to={`/movie/${item.id}`}>{item.title}</Link>;
-			});
-		}
-		return (
-			<div key={person.id} className="card person-card">
-				<div className="card--inner">
-					<Link
-						to={`/person/${person.id}`}
-						className="full-link"
-					></Link>
-					<div className="image-wrap">
-						{person.profile_path ? (
-							<LazyLoadImage
-								alt={person.name}
-								src={`https://image.tmdb.org/t/p/w200/${person.profile_path}`}
-								// effect="blur"
-							/>
-						) : (
-							<div className="no-poster"></div>
-						)}
-					</div>
-					<div className="text-wrap">
-						<p className="title">{person.name}</p>
-						{knownFor ? (
-							<p className="known-for">{knownFor}</p>
-						) : null}
-						{this.props.character ? (
-							<p className="character">{this.props.character}</p>
-						) : null}
-					</div>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    let knownFor = null;
+    let person = this.props.person;
+    if (!person || person.gender === 0) {
+      return null;
+    }
+    if (person.known_for && person.known_for.length) {
+      person.known_for.map((item, key) => {
+        return <Link to={`/movie/${item.id}`}>{item.title}</Link>;
+      });
+    }
+    return (
+      <div key={person.id} className="card person-card">
+        <div className="card--inner">
+          <Link to={`/person/${person.id}`} className="full-link"></Link>
+          <div className="image-wrap">
+            {person.profile_path ? (
+              <LazyLoadImage
+                alt={person.name}
+                src={`https://image.tmdb.org/t/p/w200/${person.profile_path}`}
+                // effect="blur"
+              />
+            ) : (
+              <div className="no-poster"></div>
+            )}
+          </div>
+          <div className="text-wrap">
+            <p className="title">{person.name}</p>
+            {knownFor ? <p className="known-for">{knownFor}</p> : null}
+            {this.props.character ? <p className="character">{this.props.character}</p> : null}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default withRouter(PersonCard);
