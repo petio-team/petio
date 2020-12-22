4cc6a8566f4e5a793585f276c2600f1953cbd28f
AshDyson <ashley@hypergram.co>
2020-12-22 18:35:35 +0000

    Upgrade settings UI to multi server

--- api/app.js
+++ api/app.js
@@ -1,266 +1,238 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv/config');
-const CronJob = require('cron').CronJob;
-const fs = require('fs');
-const path = require('path');
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv/config");
+const CronJob = require("cron").CronJob;
+const fs = require("fs");
+const path = require("path");
 
 // Config
-const getConfig = require('./util/config');
+const getConfig = require("./util/config");
 
 // Plex
-const LibraryUpdate = require('./plex/libraryUpdate');
+const LibraryUpdate = require("./plex/libraryUpdate");
 
 // Routes
-const movieRoute = require('./routes/movie');
-const showRoute = require('./routes/show');
-const searchRoute = require('./routes/search');
-const personRoute = require('./routes/person');
-const loginRoute = require('./routes/login');
-const trendingRoute = require('./routes/trending');
-const requestRoute = require('./routes/request');
-const topRoute = require('./routes/top');
-const historyRoute = require('./routes/history');
-const plexRoute = require('./routes/plex');
-const reviewRoute = require('./routes/review');
-const userRoute = require('./routes/user');
-const genieRoute = require('./routes/genie');
-const sessionsRoute = require('./routes/sessions');
+const movieRoute = require("./routes/movie");
+const showRoute = require("./routes/show");
+const searchRoute = require("./routes/search");
+const personRoute = require("./routes/person");
+const loginRoute = require("./routes/login");
+const trendingRoute = require("./routes/trending");
+const requestRoute = require("./routes/request");
+const topRoute = require("./routes/top");
+const historyRoute = require("./routes/history");
+const plexRoute = require("./routes/plex");
+const reviewRoute = require("./routes/review");
+const userRoute = require("./routes/user");
+const genieRoute = require("./routes/genie");
+const sessionsRoute = require("./routes/sessions");
 // const setupRoute = require('./routes/setup');
-const servicesRoute = require('./routes/services');
-const mailRoute = require('./routes/mail');
+const servicesRoute = require("./routes/services");
+const mailRoute = require("./routes/mail");
 
 class Main {
-	constructor() {
-		this.cron = new CronJob('0 */30 * * * *', function () {
-			const d = new Date();
-			console.log('Library Watch Running:', d);
-			new LibraryUpdate().run();
-		});
-		if (process.pkg) {
-			this.createConfigDir(
-				path.join(path.dirname(process.execPath), './config')
-			);
-		} else {
-			this.createConfigDir(path.join(__dirname, './config'));
-		}
-		this.config = getConfig();
-		this.e = app;
-		this.server = null;
-		this.e.use(cors());
-		this.e.options('*', cors());
-		this.e.use(express.json());
-		this.e.use(express.urlencoded({ extended: true }));
-	}
+  constructor() {
+    this.cron = new CronJob("0 */30 * * * *", function () {
+      const d = new Date();
+      console.log("Library Watch Running:", d);
+      new LibraryUpdate().run();
+    });
+    if (process.pkg) {
+      this.createConfigDir(path.join(path.dirname(process.execPath), "./config"));
+    } else {
+      this.createConfigDir(path.join(__dirname, "./config"));
+    }
+    this.config = getConfig();
+    this.e = app;
+    this.server = null;
+    this.e.use(cors());
+    this.e.options("*", cors());
+    this.e.use(express.json());
+    this.e.use(express.urlencoded({ extended: true }));
+  }
 
-	setRoutes() {
-		console.log('Setting up routes');
-		this.e.get('/config', async (req, res) => {
-			res.json(this.config ? { config: true } : { config: false });
-		});
-		this.setup();
-		if (this.config) {
-			this.e.use('/login', loginRoute);
-			this.e.use('/movie', movieRoute);
-			this.e.use('/show', showRoute);
-			this.e.use('/person', personRoute);
-			this.e.use('/search', searchRoute);
-			this.e.use('/trending', trendingRoute);
-			this.e.use('/request', requestRoute);
-			this.e.use('/top', topRoute);
-			this.e.use('/history', historyRoute);
-			this.e.use('/plex', plexRoute);
-			this.e.use('/review', reviewRoute);
-			this.e.use('/user', userRoute);
-			this.e.use('/genie', genieRoute);
-			this.e.use('/sessions', sessionsRoute);
-			this.e.use('/services', servicesRoute);
-			this.e.use('/mail', mailRoute);
-		}
-	}
+  setRoutes() {
+    console.log("Setting up routes");
+    this.e.get("/config", async (req, res) => {
+      res.json(this.config ? { config: true } : { config: false });
+    });
+    this.setup();
+    if (this.config) {
+      this.e.use("/login", loginRoute);
+      this.e.use("/movie", movieRoute);
+      this.e.use("/show", showRoute);
+      this.e.use("/person", personRoute);
+      this.e.use("/search", searchRoute);
+      this.e.use("/trending", trendingRoute);
+      this.e.use("/request", requestRoute);
+      this.e.use("/top", topRoute);
+      this.e.use("/history", historyRoute);
+      this.e.use("/plex", plexRoute);
+      this.e.use("/review", reviewRoute);
+      this.e.use("/user", userRoute);
+      this.e.use("/genie", genieRoute);
+      this.e.use("/sessions", sessionsRoute);
+      this.e.use("/services", servicesRoute);
+      this.e.use("/mail", mailRoute);
+    }
+  }
 
-	async restart() {
-		console.log('Restarting server');
-		this.cron.stop();
-		await this.server.close();
-		this.config = getConfig();
-		this.init();
-	}
+  async restart() {
+    console.log("Restarting server");
+    this.cron.stop();
+    await this.server.close();
+    this.config = getConfig();
+    this.init();
+  }
 
-	init() {
-		this.setRoutes();
-		console.log('Starting Server ');
-		this.server = this.e.listen(7778);
-		console.log('Listening on 7778');
-		if (!this.config) {
-			console.log('No config, entering setup mode');
-		} else {
-			// Routing
-			console.log('Connecting to Database, please wait....');
-			this.connectDb();
-		}
-	}
+  init() {
+    this.setRoutes();
+    console.log("Starting Server ");
+    this.server = this.e.listen(7778);
+    console.log("Listening on 7778");
+    if (!this.config) {
+      console.log("No config, entering setup mode");
+    } else {
+      // Routing
+      console.log("Connecting to Database, please wait....");
+      this.connectDb();
+    }
+  }
 
-	async connectDb() {
-		try {
-			await mongoose.connect(this.config.DB_URL, {
-				useNewUrlParser: true,
-				useUnifiedTopology: true,
-			});
-			console.log('Connected to Database ');
-			this.start();
-		} catch (err) {
-			console.log(err);
-			console.log('Fatal error - database misconfigured!');
-			console.log('Removing config please restart');
-			fs.unlinkSync('./config/config.json');
-		}
-	}
+  async connectDb() {
+    try {
+      await mongoose.connect(this.config.DB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log("Connected to Database ");
+      this.start();
+    } catch (err) {
+      console.log(err);
+      console.log("Fatal error - database misconfigured!");
+      console.log("Removing config please restart");
+      fs.unlinkSync("./config/config.json");
+    }
+  }
 
-	async start() {
-		const libUpdate = new LibraryUpdate();
-		this.cron.start();
-		libUpdate.run();
-	}
+  async start() {
+    const libUpdate = new LibraryUpdate();
+    this.cron.start();
+    libUpdate.run();
+  }
 
-	setup() {
-		this.e.post('/setup/set', async (req, res) => {
-			if (this.config) {
-				res.status(403).send('Config exists');
-				console.log(
-					'Error: Config creation blocked, config already exists, this is likely malicious'
-				);
-				return;
-			}
-			let user = req.body.user;
-			let server = req.body.server;
-			let db = req.body.db;
-			if (!user || !server || !db) {
-				res.status(500).send('Missing Fields');
-				return;
-			}
+  setup() {
+    this.e.post("/setup/set", async (req, res) => {
+      if (this.config) {
+        res.status(403).send("Config exists");
+        console.log("Error: Config creation blocked, config already exists, this is likely malicious");
+        return;
+      }
+      let user = req.body.user;
+      let server = req.body.server;
+      let db = req.body.db;
+      if (!user || !server || !db) {
+        res.status(500).send("Missing Fields");
+        return;
+      }
 
-			let configData = {
-				DB_URL: db + '/petio',
-				tmdbApi: 'a9a99e29e94d33f6a9a3bb78c7a450f7',
-				plexProtocol: server.protocol,
-				plexIp: server.host,
-				plexPort: server.port,
-				plexToken: user.token,
-				adminUsername: user.username,
-				adminEmail: user.email,
-				adminPass: user.password,
-				adminId: user.id,
-				adminThumb: user.thumb,
-				adminDisplayName: user.username,
-				fanartApi: '930d724053d35fcc01a1a6da58fbb80a',
-			};
-			try {
-				await this.createConfig(JSON.stringify(configData, null, 2));
-				await this.createDefaults();
-				res.send('Config Created');
-				console.log('Config Created');
-				this.restart();
-				return;
-			} catch (err) {
-				res.status(500).send('Error Creating config');
-				console.log('Config creation error');
-				console.log(err);
-			}
-		});
-	}
+      let configData = {
+        DB_URL: db + "/petio",
+        tmdbApi: "a9a99e29e94d33f6a9a3bb78c7a450f7",
+        plexProtocol: server.protocol,
+        plexIp: server.host,
+        plexPort: server.port,
+        plexToken: user.token,
+        adminUsername: user.username,
+        adminEmail: user.email,
+        adminPass: user.password,
+        adminId: user.id,
+        adminThumb: user.thumb,
+        adminDisplayName: user.username,
+        fanartApi: "930d724053d35fcc01a1a6da58fbb80a",
+      };
+      try {
+        await this.createConfig(JSON.stringify(configData, null, 2));
+        await this.createDefaults();
+        res.send("Config Created");
+        console.log("Config Created");
+        this.restart();
+        return;
+      } catch (err) {
+        res.status(500).send("Error Creating config");
+        console.log("Config creation error");
+        console.log(err);
+      }
+    });
+  }
 
-	createConfig(data) {
-		return new Promise((resolve, reject) => {
-			let project_folder, configFile;
-			if (process.pkg) {
-				project_folder = path.dirname(process.execPath);
-				configFile = path.join(project_folder, './config/config.json');
-			} else {
-				project_folder = __dirname;
-				configFile = path.join(project_folder, './config/config.json');
-			}
-			console.log(configFile);
-			fs.writeFile(configFile, data, (err) => {
-				if (err) {
-					console.log(err);
-					reject(err);
-					console.log('Config Failed');
-				} else {
-					console.log(data);
-					resolve();
-					console.log('Config Created');
-				}
-			});
-		});
-	}
+  createConfig(data) {
+    return new Promise((resolve, reject) => {
+      let project_folder, configFile;
+      if (process.pkg) {
+        project_folder = path.dirname(process.execPath);
+        configFile = path.join(project_folder, "./config/config.json");
+      } else {
+        project_folder = __dirname;
+        configFile = path.join(project_folder, "./config/config.json");
+      }
+      console.log(configFile);
+      fs.writeFile(configFile, data, (err) => {
+        if (err) {
+          console.log(err);
+          reject(err);
+          console.log("Config Failed");
+        } else {
+          console.log(data);
+          resolve();
+          console.log("Config Created");
+        }
+      });
+    });
+  }
 
-	async createDefaults() {
-		let project_folder = __dirname;
-		let email = process.pkg
-			? path.join(path.dirname(process.execPath), './config/email.json')
-			: path.join(project_folder, './config/email.json');
-		let emailDefault = JSON.stringify({
-			emailUser: '',
-			emailPass: '',
-			emailServer: '',
-			emailPort: '',
-			emailSecure: false,
-		});
+  async createDefaults() {
+    let project_folder = __dirname;
+    let email = process.pkg ? path.join(path.dirname(process.execPath), "./config/email.json") : path.join(project_folder, "./config/email.json");
+    let emailDefault = JSON.stringify({
+      emailUser: "",
+      emailPass: "",
+      emailServer: "",
+      emailPort: "",
+      emailSecure: false,
+    });
 
-		let radarr = process.pkg
-			? path.join(path.dirname(process.execPath), './config/radarr.json')
-			: path.join(project_folder, './config/radarr.json');
-		let radarrDefault = JSON.stringify({
-			enabled: false,
-			protocol: 'http',
-			hostname: '',
-			apiKey: '',
-			port: false,
-			urlBase: '',
-			rootPath: '',
-			profileId: false,
-		});
+    let radarr = process.pkg ? path.join(path.dirname(process.execPath), "./config/radarr.json") : path.join(project_folder, "./config/radarr.json");
+    let radarrDefault = JSON.stringify([]);
 
-		let sonarr = process.pkg
-			? path.join(path.dirname(process.execPath), './config/sonarr.json')
-			: path.join(project_folder, './config/sonarr.json');
-		let sonarrDefault = JSON.stringify({
-			enabled: false,
-			apiKey: '',
-			hostname: '',
-			port: '',
-			profileId: '',
-			protocol: 'http',
-			rootPath: '',
-			urlBase: '',
-		});
-		try {
-			await fs.writeFileSync(email, emailDefault);
-			await fs.writeFileSync(radarr, radarrDefault);
-			await fs.writeFileSync(sonarr, sonarrDefault);
+    let sonarr = process.pkg ? path.join(path.dirname(process.execPath), "./config/sonarr.json") : path.join(project_folder, "./config/sonarr.json");
+    let sonarrDefault = JSON.stringify([]);
+    try {
+      await fs.writeFileSync(email, emailDefault);
+      await fs.writeFileSync(radarr, radarrDefault);
+      await fs.writeFileSync(sonarr, sonarrDefault);
 
-			return;
-		} catch (err) {
-			console.log('Fatal Error: Cannot create default configs');
-			throw err;
-		}
-	}
+      return;
+    } catch (err) {
+      console.log("Fatal Error: Cannot create default configs");
+      throw err;
+    }
+  }
 
-	createConfigDir(dir) {
-		return new Promise((resolve, reject) => {
-			console.log('Attempting to create config dir');
-			if (fs.existsSync(dir)) {
-				resolve();
-				return true;
-			}
-			fs.mkdirSync(dir);
-			console.log('Config Directory Created');
-			resolve();
-		});
-	}
+  createConfigDir(dir) {
+    return new Promise((resolve, reject) => {
+      console.log("Attempting to create config dir");
+      if (fs.existsSync(dir)) {
+        resolve();
+        return true;
+      }
+      fs.mkdirSync(dir);
+      console.log("Config Directory Created");
+      resolve();
+    });
+  }
 }
 
 const API = new Main();
--- api/services/sonarr.js
+++ api/services/sonarr.js
@@ -1,206 +1,198 @@
-const request = require('xhr-request');
-const Request = require('../models/request');
-const fs = require('fs');
-const path = require('path');
-var sanitize = require('sanitize-filename');
+const request = require("xhr-request");
+const Request = require("../models/request");
+const fs = require("fs");
+const path = require("path");
+var sanitize = require("sanitize-filename");
 
 class Sonarr {
-	constructor() {
-		let project_folder, configFile;
-		if (process.pkg) {
-			project_folder = path.dirname(process.execPath);
-			configFile = path.join(project_folder, './config/sonarr.json');
-		} else {
-			project_folder = __dirname;
-			configFile = path.join(project_folder, '../config/sonarr.json');
-		}
-		const configData = fs.readFileSync(configFile);
-		const configParse = JSON.parse(configData);
-		this.config = configParse;
-	}
+  constructor(id = false) {
+    let project_folder, configFile;
+    if (process.pkg) {
+      project_folder = path.dirname(process.execPath);
+      configFile = path.join(project_folder, "./config/sonarr.json");
+    } else {
+      project_folder = __dirname;
+      configFile = path.join(project_folder, "../config/sonarr.json");
+    }
+    const configData = fs.readFileSync(configFile);
+    const configParse = JSON.parse(configData);
+    this.fullConfig = configParse;
+    if (id) {
+      this.config = configParse[id];
+    }
+  }
 
-	process(method, endpoint, params, body = false) {
-		return new Promise((resolve, reject) => {
-			if (!this.config.hostname) {
-				reject('');
-				return;
-			}
-			if (!params) {
-				params = {};
-			}
-			params.apikey = this.config.apiKey;
-			let paramsString = '';
-			Object.keys(params).map((val, i) => {
-				let key = val;
-				paramsString += `${i === 0 ? '?' : '&'}${key}=${params[val]}`;
-			});
-			let url = `${this.config.protocol}://${this.config.hostname}${
-				this.config.port ? ':' + this.config.port : ''
-			}${this.config.urlBase}/api/${endpoint}${paramsString}`;
-			let args = {
-				method: method,
-				json: true,
-			};
-			if (method === 'post' && body) {
-				args = {
-					method: method,
-					json: true,
-					body: body,
-				};
-			}
-			request(url, args, function (err, data) {
-				if (err) {
-					reject(err);
-				} else {
-					resolve(data);
-				}
-			});
-		});
-	}
+  process(method, endpoint, params, body = false) {
+    return new Promise((resolve, reject) => {
+      if (!this.config.hostname) {
+        reject("");
+        return;
+      }
+      if (!params) {
+        params = {};
+      }
+      params.apikey = this.config.apiKey;
+      let paramsString = "";
+      Object.keys(params).map((val, i) => {
+        let key = val;
+        paramsString += `${i === 0 ? "?" : "&"}${key}=${params[val]}`;
+      });
+      let url = `${this.config.protocol}://${this.config.hostname}${this.config.port ? ":" + this.config.port : ""}${this.config.urlBase}/api/${endpoint}${paramsString}`;
+      let args = {
+        method: method,
+        json: true,
+      };
+      if (method === "post" && body) {
+        args = {
+          method: method,
+          json: true,
+          body: body,
+        };
+      }
+      request(url, args, function (err, data) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      });
+    });
+  }
 
-	getConfig() {
-		return this.config;
-	}
+  getConfig() {
+    return this.fullConfig;
+  }
 
-	async get(endpoint, params = false) {
-		return this.process('get', endpoint, params);
-	}
+  async get(endpoint, params = false) {
+    return this.process("get", endpoint, params);
+  }
 
-	async post(endpoint, params = false, body = {}) {
-		return this.process('post', endpoint, params, body);
-	}
+  async post(endpoint, params = false, body = {}) {
+    return this.process("post", endpoint, params, body);
+  }
 
-	async connect(test = false) {
-		if (!this.config.enabled && !test) {
-			console.log('SERVICE - SONARR: Sonarr not enabled');
-			return false;
-		}
-		try {
-			let check = await this.get('system/status');
-			if (check.error) {
-				console.log('SERVICE - SONARR: ERR Connection failed');
-				return false;
-			}
-			if (check) {
-				return true;
-			} else {
-				console.log('SERVICE - SONARR: ERR Connection failed');
-				return false;
-			}
-		} catch (err) {
-			console.log('SERVICE - SONARR: ERR Connection failed');
-			return false;
-		}
-	}
+  async connect(test = false) {
+    if (!this.config.active && !test) {
+      console.log("SERVICE - SONARR: Sonarr not enabled");
+      return false;
+    }
+    try {
+      let check = await this.get("system/status");
+      if (check.error) {
+        console.log("SERVICE - SONARR: ERR Connection failed");
+        return false;
+      }
+      if (check) {
+        return true;
+      } else {
+        console.log("SERVICE - SONARR: ERR Connection failed");
+        return false;
+      }
+    } catch (err) {
+      console.log("SERVICE - SONARR: ERR Connection failed");
+      return false;
+    }
+  }
 
-	async getPaths() {
-		return await this.get('rootfolder');
-	}
+  async getPaths() {
+    return await this.get("rootfolder");
+  }
 
-	async getProfiles() {
-		return await this.get('profile');
-	}
+  async getProfiles() {
+    return await this.get("profile");
+  }
 
-	lookup(id) {
-		return this.get('series/lookup', {
-			term: `tvdb:${id}`,
-		});
-	}
+  lookup(id) {
+    return this.get("series/lookup", {
+      term: `tvdb:${id}`,
+    });
+  }
 
-	async series(id) {
-		const active = await this.connect();
-		if (!active) {
-			return false;
-		}
-		return this.get(`series/${id}`);
-	}
+  async series(id) {
+    const active = await this.connect();
+    if (!active) {
+      return false;
+    }
+    return this.get(`series/${id}`);
+  }
 
-	async queue() {
-		const active = await this.connect();
-		if (!active) {
-			return false;
-		}
-		return this.get(`queue`);
-	}
+  async queue() {
+    const active = await this.connect();
+    if (!active) {
+      return false;
+    }
+    return this.get(`queue`);
+  }
 
-	async test() {
-		return await this.connect(true);
-	}
+  async test() {
+    return await this.connect(true);
+  }
 
-	async add(seriesData) {
-		seriesData.ProfileId = this.config.profileId;
-		seriesData.Path = `${this.config.rootPath}${sanitize(
-			seriesData.title
-		)} (${seriesData.year})`;
-		seriesData.addOptions = {
-			searchForMissingEpisodes: true,
-		};
+  async add(seriesData) {
+    seriesData.ProfileId = this.config.profileId;
+    seriesData.Path = `${this.config.rootPath}${sanitize(seriesData.title)} (${seriesData.year})`;
+    seriesData.addOptions = {
+      searchForMissingEpisodes: true,
+    };
 
-		try {
-			let add = await this.post('series', false, seriesData);
-			console.log(add);
-			return add.id;
-		} catch (err) {
-			console.log(err);
-			console.log(`SERVICE - SONARR: Unable to add series ${err}`);
-			return false;
-		}
-	}
+    try {
+      let add = await this.post("series", false, seriesData);
+      console.log(add);
+      return add.id;
+    } catch (err) {
+      console.log(err);
+      console.log(`SERVICE - SONARR: Unable to add series ${err}`);
+      return false;
+    }
+  }
 
-	async processJobs(jobQ) {
-		for (let job of jobQ) {
-			try {
-				let sonarrData = await this.lookup(job.tvdb_id);
-				let sonarrId = await this.add(sonarrData[0]);
-				let updatedRequest = await Request.findOneAndUpdate(
-					{
-						sonarrId: sonarrId,
-					},
-					{
-						$set: {
-							sonarrId: sonarrId,
-						},
-					},
-					{ useFindAndModify: false }
-				);
-				if (updatedRequest) {
-					console.log(
-						`SERVICE - SONARR: Sonnar job added for ${job.title}`
-					);
-				}
-			} catch (err) {
-				console.log(
-					`SERVICE - SONARR: Unable to add series ${job.title}`
-				);
-			}
-		}
-	}
+  async processJobs(jobQ) {
+    for (let job of jobQ) {
+      try {
+        let sonarrData = await this.lookup(job.tvdb_id);
+        let sonarrId = await this.add(sonarrData[0]);
+        let updatedRequest = await Request.findOneAndUpdate(
+          {
+            sonarrId: sonarrId,
+          },
+          {
+            $set: {
+              sonarrId: sonarrId,
+            },
+          },
+          { useFindAndModify: false }
+        );
+        if (updatedRequest) {
+          console.log(`SERVICE - SONARR: Sonnar job added for ${job.title}`);
+        }
+      } catch (err) {
+        console.log(`SERVICE - SONARR: Unable to add series ${job.title}`);
+      }
+    }
+  }
 
-	async getRequests() {
-		console.log(`SERVICE - SONARR: Polling requests`);
-		const active = await this.connect();
-		if (!active) {
-			console.log(`SERVICE - SONARR: Connection Failed Stopping Poll`);
-			return;
-		}
-		const requests = await Request.find();
-		let jobQ = [];
-		for (let req of requests) {
-			if (req.type === 'tv') {
-				if (!req.tvdb_id) {
-					console.log(
-						`SERVICE - SONARR: TVDB ID not found for ${req.title}`
-					);
-				} else if (!req.sonarrId) {
-					jobQ.push(req);
-					console.log(
-						`SERVICE - SONARR: ${req.title} added to job queue`
-					);
-				}
-			}
-		}
-		this.processJobs(jobQ);
-	}
+  async getRequests() {
+    return false;
+    console.log(`SERVICE - SONARR: Polling requests`);
+    const active = await this.connect();
+    if (!active) {
+      console.log(`SERVICE - SONARR: Connection Failed Stopping Poll`);
+      return;
+    }
+    const requests = await Request.find();
+    let jobQ = [];
+    for (let req of requests) {
+      if (req.type === "tv") {
+        if (!req.tvdb_id) {
+          console.log(`SERVICE - SONARR: TVDB ID not found for ${req.title}`);
+        } else if (!req.sonarrId) {
+          jobQ.push(req);
+          console.log(`SERVICE - SONARR: ${req.title} added to job queue`);
+        }
+      }
+    }
+    this.processJobs(jobQ);
+  }
 }
 
 module.exports = Sonarr;
