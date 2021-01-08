cd43087c401cb3cd46bc91ef14c5fdca366b0919
AshDyson <ashley@hypergram.co>
2021-01-08 14:32:41 +0000

    sub dir api fix

--- frontend/src/data/auth/index.js
+++ frontend/src/data/auth/index.js
@@ -1,26 +1,25 @@
-import { store } from '../store';
-import * as types from '../actionTypes';
+import { store } from "../store";
+import * as types from "../actionTypes";
 
 const PlexRequestApi =
-	process.env.NODE_ENV === 'development' ||
-	process.env.REACT_APP_ENV === 'pkg'
-		? 'http://localhost:7778'
-		: `${window.location.protocol}//${window.location.host}/api`;
+  process.env.NODE_ENV === "development" || process.env.REACT_APP_ENV === "pkg"
+    ? "http://localhost:7778"
+    : `${window.location.protocol}//${window.location.host}${window.location.pathname.replace("/admin", "")}/api`;
 
 export function initAuth() {
-	finalise({
-		type: types.CREDENTIALS,
-		credentials: {
-			api: PlexRequestApi,
-		},
-	});
+  finalise({
+    type: types.CREDENTIALS,
+    credentials: {
+      api: PlexRequestApi,
+    },
+  });
 }
 
 export function getAuth() {
-	return store.getState().user.credentials;
+  return store.getState().user.credentials;
 }
 
 function finalise(data = false) {
-	if (!data) return false;
-	return store.dispatch(data);
+  if (!data) return false;
+  return store.dispatch(data);
 }
