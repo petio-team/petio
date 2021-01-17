d666f5fd5929e5ee70b8a6f204acd2ec5b7d1426
AshDyson <ashley@hypergram.co>
2021-01-17 19:30:30 +0000

    Comment / tidy wrapper

--- petio.js
+++ petio.js
@@ -6,6 +6,7 @@ const API = require("./api/app");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 class Wrapper {
+  // Start Main Wrapper
   init() {
     console.log("Starting Petio");
     this.admin();
@@ -13,9 +14,9 @@ class Wrapper {
     this.api();
     this.notfound();
     app.listen(7777);
-    // open("http://localhost:7777/admin/");
   }
 
+  // Proxy API on port 7778 internally
   api() {
     app.use(
       "/api",
@@ -28,16 +29,19 @@ class Wrapper {
     );
   }
 
+  // Serve Admin files
   admin() {
     const adminPath = process.pkg ? path.join(path.dirname(process.execPath), "./views/admin") : path.join(__dirname, "./views/admin");
     app.use("/admin/", express.static(adminPath));
   }
 
+  // Server user frontend files
   user() {
     const fePath = process.pkg ? path.join(path.dirname(process.execPath), "./views/frontend") : path.join(__dirname, "./views/frontend");
     app.use("/", express.static(fePath));
   }
 
+  // Catch 404s at main router level
   notfound() {
     app.get("*", function (req, res) {
       res.status(404).send("Petio Router: not found");
