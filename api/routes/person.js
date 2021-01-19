e3aa6d32dd879f176f114f36a8f7cf1080b73f33
AshDyson <ashley@hypergram.co>
2021-01-19 12:54:14 +0000

    Server side caching

--- api/routes/person.js
+++ api/routes/person.js
@@ -1,10 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const personLookup = require('../tmdb/person');
+const personLookup = require("../tmdb/person");
 
-router.get('/lookup/:id', async (req, res) => {
-	let data = await personLookup(req.params.id);
-	res.json(data);
+const ExpressCache = require("express-cache-middleware");
+const cacheManager = require("cache-manager");
+
+// Cache for 1 day
+const cacheMiddleware = new ExpressCache(
+  cacheManager.caching({
+    store: "memory",
+    max: 100,
+    ttl: 86400,
+  })
+);
+
+cacheMiddleware.attach(router);
+
+router.get("/lookup/:id", async (req, res) => {
+  let data = await personLookup(req.params.id);
+  res.json(data);
 });
 
 module.exports = router;
