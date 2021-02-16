f1b0fb133b02a0bb6fa6356bf98d3e5e1f52f2ae
Michael Goodnow <mmgoodnow@gmail.com>
2021-02-15 20:23:41 -0500

    Refactor api functions in client

--- router.js
+++ router.js
@@ -5,40 +5,42 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 const logger = require("./api/util/logger");
 
 const adminPath = process.pkg
-  ? path.join(path.dirname(process.execPath), "./views/admin")
-  : path.join(__dirname, "./views/admin");
+	? path.join(path.dirname(process.execPath), "./views/admin")
+	: path.join(__dirname, "./views/admin");
 logger.log("verbose", `ROUTER: Serving admin route to ${adminPath}`);
-router.use("/admin/", express.static(adminPath));
+router.use("/admin", express.static(adminPath));
 
 const fePath = process.pkg
-  ? path.join(path.dirname(process.execPath), "./views/frontend")
-  : path.join(__dirname, "./views/frontend");
+	? path.join(path.dirname(process.execPath), "./views/frontend")
+	: path.join(__dirname, "./views/frontend");
 logger.log("verbose", `ROUTER: Serving frontend route to ${fePath}`);
 router.use("/", express.static(fePath));
 
 router.use(
-  "/api",
-  createProxyMiddleware({
-    target: "http://localhost:7778",
-    pathRewrite: function (path, req) {
-      if (req.basePath !== "/") {
-        return path.replace("//", "/").replace(`${req.basePath}/api/`, "/");
-      } else {
-        return path.replace("/api/", "/");
-      }
-    },
-  })
+	"/api",
+	createProxyMiddleware({
+		target: "http://localhost:7778",
+		pathRewrite: function (path, req) {
+			if (req.basePath !== "/") {
+				return path
+					.replace("//", "/")
+					.replace(`${req.basePath}/api/`, "/");
+			} else {
+				return path.replace("/api/", "/");
+			}
+		},
+	})
 );
 logger.log("verbose", `ROUTER: API proxy setup - Proxying /api -> /`);
 
 router.get("*", function (req, res) {
-  logger.log(
-    "warn",
-    `ROUTER: Not found - ${req.path} | IP: ${
-      req.headers["x-forwarded-for"] || req.connection.remoteAddress
-    }`
-  );
-  res.status(404).send(`Petio Router: not found - ${req.path}`);
+	logger.log(
+		"warn",
+		`ROUTER: Not found - ${req.path} | IP: ${
+			req.headers["x-forwarded-for"] || req.connection.remoteAddress
+		}`
+	);
+	res.status(404).send(`Petio Router: not found - ${req.path}`);
 });
 
 module.exports = router;
