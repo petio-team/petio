0ea8e8444aeabe1906ad7b4120f337cc4dce93e4
Angry Cuban <angrycuban@angrycuban.xyz>
2021-03-08 08:17:21 -0600

    Updating Radarr/Sonarr description

--- admin/src/page/settings/radarr.js
+++ admin/src/page/settings/radarr.js
@@ -470,12 +470,7 @@ class Radarr extends React.Component {
         <section>
           <p className="main-title mb--2">Radarr</p>
           <p className="description">
-            Radarr is a movie collection manager for Usenet and BitTorrent
-            users. It can monitor multiple RSS feeds for new movies and will
-            interface with clients and indexers to grab, sort, and rename them.
-            It can also be configured to automatically upgrade the quality of
-            existing files in the library when a better quality format becomes
-            available.
+            Radarr is a DVR. It can monitor multiple RSS feeds for new movies and will grab, sort, and rename them.
           </p>
         </section>
         <section>
