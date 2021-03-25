95b349bbbe0eb2ac8734c60b9a920eddba696c62
AshDyson <ashley@hypergram.co>
2021-03-25 15:32:57 +0000

    Set path on FE cookie delete

--- frontend/src/data/User/actions.js
+++ frontend/src/data/User/actions.js
@@ -22,7 +22,7 @@ function getCookie(cname) {
 
 function deleteCookie(name) {
   if (getCookie(name)) {
-    document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:01 GMT`;
+    document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/`;
   }
 }
 
