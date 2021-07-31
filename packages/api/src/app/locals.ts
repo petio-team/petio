69cf46f1f9b9a1a46d464626d4bcd2b623d16429
ADRFranklin <adrfranklin95@gmail.com>
2021-07-31 08:46:31 +0100

    feat(locals): added local variables populated by optional env vars

---
+++ packages/api/src/app/locals.ts
@@ -0,0 +1,33 @@
+import * as env from "env-var";
+
+import appPath from "app-root-path";
+import { config } from "dotenv";
+import path from "path";
+import xdg from "xdg-app-paths";
+
+config({
+  path: path.join(appPath.toString(), ".env"),
+});
+
+export default {
+  APP_DIR: appPath.toString(),
+  HOST: env.get("HOST").default("localhost").asString(),
+  PORT: env.get("PORT").default(7777).asPortNumber(),
+  BASE_PATH: env.get("BASE_PATH").default("/").asString(),
+  LOG_LEVEL: env
+    .get("LOG_LEVEL")
+    .default("info")
+    .asEnum(["info", "warning", "error", "debug"]),
+  IS_PROD: env.get("NODE_ENV").asString() === "production",
+  MONGODB_URL: env
+    .get("MONGODB_URL")
+    .default("mongodb://localhost:27017/petio")
+    .asString(),
+  CONFIG_DIR: env
+    .get("CONFIG_DIR")
+    .default(path.join(xdg.config(), "petio"))
+    .asString(),
+  TRUSTED_PROXIES: env.get("TRUSTED_PROXIES").default("").asString().split(","),
+  TMDB_API_KEY: env.get("TMDB_API_KEY").default("1af5ad19a2d972a67cd27eb033979c4c").asString(),
+  FANART_API_KEY: env.get("FANART_API_KEY").default("ee409f6fb0c5cd2352e7a454d3f580d4").asString(),
+};
