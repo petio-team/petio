962081a4d13517bbd8bb9c415e4953a0051d40ff
ADRFranklin <adrfranklin95@gmail.com>
2022-01-20 18:03:25 +0000

    chore: add env,puid,pgid to the environment

--- pkg/api/app/env.js
+++ pkg/api/app/env.js
@@ -4,6 +4,9 @@ const ROOT_DIR = process.pkg ? path.dirname(process.execPath) : process.cwd();
 
 module.exports = {
     rootDir: ROOT_DIR,
+    env: process.env.NODE_ENV ?? "production",
+    puid: parseInt(process.env.PUID) ?? 1000,
+    pgid: parseInt(process.env.PGID) ?? 1000,
     configFolder: process.env.CONFIG_FOLDER ?? path.join(ROOT_DIR, "./config"),
     logsFolder: process.env.LOGS_FOLDER ?? path.join(ROOT_DIR, "./logs"),
 };
