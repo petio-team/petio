f51df71c48643309bff4f9942398aeeceea16348
ADRFranklin <adrfranklin95@gmail.com>
2022-01-21 09:00:33 +0000

    chore: cleaned up main try/catch block

--- pkg/api/main.js
+++ pkg/api/main.js
@@ -7,27 +7,26 @@ const App = require('./app/app');
 const Worker = require("./worker");
 const logger = require("./app/logger");
 const { loadConfig } = require("./app/config");
-const checkPerms = require("./util/perms");
+const doPerms = require("./util/perms");
 
 try {
   // attempt to check config/logs folders are readable/writable else try to make them readable/writable
   // throws error if fails
-  checkPerms();
+  doPerms();
   // load config
   loadConfig();
-} catch (e) {
-  logger.log(e);
-  process.exit(1);
-}
-
-// check if healthcheck flag is set and run healthcheck
-const args = process.argv.slice(2).filter((arg) => arg == "--healthcheck");
-if (args.length) {
-  require("./healthcheck");
-} else {
-  if (cluster.isPrimary) {
-    App();
+  // check if healthcheck flag is set and run healthcheck
+  const args = process.argv.slice(2).filter((arg) => arg == "--healthcheck");
+  if (args.length) {
+    require("./healthcheck");
   } else {
-    new Worker().startCrons();
+    if (cluster.isPrimary) {
+      App();
+    } else {
+      new Worker().startCrons();
+    }
   }
+} catch (e) {
+  logger.error(e);
+  process.exit(1);
 }
