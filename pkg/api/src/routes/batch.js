9c5b412c406391b0a25022bc30b31cd746ca98c5
AshDyson <ashleyrobert@hotmail.co.uk>
2022-01-24 13:25:40 +0000

    feat: Show batch lookup

--- pkg/api/src/routes/batch.js
+++ pkg/api/src/routes/batch.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const logger = require("../app/logger");
 const { movieLookup } = require("../tmdb/movie");
-const showLookup = require("../tmdb/show");
+const { showLookup } = require("../tmdb/show");
 
 router.post("/movie", async (req, res) => {
   const ids = req.body.ids;
@@ -10,6 +10,18 @@ router.post("/movie", async (req, res) => {
     ids.map(async (id) => {
       if (!id) return;
       const movie = await movieLookup(id, true);
+      return movie;
+    })
+  );
+  res.json(output);
+});
+
+router.post("/tv", async (req, res) => {
+  const ids = req.body.ids;
+  let output = await Promise.all(
+    ids.map(async (id) => {
+      if (!id) return;
+      const movie = await showLookup(id, true);
       return movie;
     })
   );
