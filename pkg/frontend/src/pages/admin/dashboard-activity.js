0a4bf3e0b9a6676884bea8b05ef1993d29c8aaa7
AshDyson <ashleyrobert@hotmail.co.uk>
2022-01-30 00:45:26 +0000

    admin sessions - no playback message

--- pkg/frontend/src/pages/admin/dashboard-activity.js
+++ pkg/frontend/src/pages/admin/dashboard-activity.js
@@ -3,6 +3,7 @@ import styles from "../../styles/views/admin.module.scss";
 import { getSessions } from "../../services/plex.service";
 import { useEffect, useState } from "react";
 import ActivitySession from "./activitySession";
+import typo from "../../styles/components/typography.module.scss";
 
 const mapStateToProps = (state) => {
   return {
@@ -42,54 +43,60 @@ function Activity({ system }) {
 
   return (
     <div className={styles.dashboard__activity__wrap}>
-      {system.sessions
-        ? system.sessions.map((session) => {
-            let media = null;
-            let selectedMedia = false;
-            let type = false;
-            for (let item of session.Media) {
-              if (item.selected) {
-                selectedMedia = item;
-              }
+      {system.sessions && system.session.length > 0 ? (
+        system.sessions.map((session) => {
+          let media = null;
+          let selectedMedia = false;
+          let type = false;
+          for (let item of session.Media) {
+            if (item.selected) {
+              selectedMedia = item;
             }
+          }
 
-            let playback = selectedMedia.Part
-              ? selectedMedia.Part[0].decision
-              : "unknown";
-            switch (session.type) {
-              case "episode":
-                media = session.grandparentRatingKey;
-                type = "tv";
-                break;
-              case "movie":
-                media = session.ratingKey;
-                type = "movie";
-                break;
-              default:
-                media = null;
-                if (!media) return null;
-            }
-            return (
-              <div
-                key={`activity_sesion_${session.ratingKey}`}
-                className={styles.dashboard__activity__item}
-              >
-                <ActivitySession
-                  ratingKey={media}
-                  type={type}
-                  session={session}
-                  progress={
-                    session.Media.length > 0 && session.viewOffset
-                      ? (session.viewOffset / session.Media[0].duration) * 100
-                      : 0
-                  }
-                  selectedMedia={selectedMedia}
-                  playback={playback}
-                />
-              </div>
-            );
-          })
-        : null}
+          let playback = selectedMedia.Part
+            ? selectedMedia.Part[0].decision
+            : "unknown";
+          switch (session.type) {
+            case "episode":
+              media = session.grandparentRatingKey;
+              type = "tv";
+              break;
+            case "movie":
+              media = session.ratingKey;
+              type = "movie";
+              break;
+            default:
+              media = null;
+              if (!media) return null;
+          }
+          return (
+            <div
+              key={`activity_sesion_${session.ratingKey}`}
+              className={styles.dashboard__activity__item}
+            >
+              <ActivitySession
+                ratingKey={media}
+                type={type}
+                session={session}
+                progress={
+                  session.Media.length > 0 && session.viewOffset
+                    ? (session.viewOffset / session.Media[0].duration) * 100
+                    : 0
+                }
+                selectedMedia={selectedMedia}
+                playback={playback}
+              />
+            </div>
+          );
+        })
+      ) : (
+        <div className={styles.dashboard__module}>
+          <p className={`${typo.body} ${typo.medium}`}>
+            Nothing is currently being played.
+          </p>
+        </div>
+      )}
     </div>
   );
 }
