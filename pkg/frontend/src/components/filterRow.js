e6cf1ddded1d41af29f6eb5e0dc98e08ebaf7e34
ADRFranklin <adrfranklin95@gmail.com>
2022-02-23 08:32:40 +0000

    fix: return a value at the end of map block

--- pkg/frontend/src/components/filterRow.js
+++ pkg/frontend/src/components/filterRow.js
@@ -221,6 +221,7 @@ export default function FilterRow(props) {
                       </option>
                     );
                   }
+                  return null;
                 })}
               </>
             ) : (
