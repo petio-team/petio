d99e8d9e0ebe800263928db1125f0ca8711aa9f0
ADRFranklin <adrfranklin95@gmail.com>
2022-03-07 05:03:19 +0000

    fix(tmdb): fix changes made to zodios client

--- pkg/api/src/tmdb/key.ts
+++ pkg/api/src/tmdb/key.ts
@@ -9,17 +9,16 @@ function createRequestInterceptor(provider: ApiKeyPluginConfig) {
   return async (config: AxiosRequestConfig) => {
     config.params = {
       ...config.params,
-      "api_key": await provider.getApiKey(),
+      api_key: await provider.getApiKey(),
     };
     return config;
   };
 }
 
-export function pluginApiKey<
-  Url extends string,
-  Api extends ZodiosEnpointDescriptions
->(provider: ApiKeyPluginConfig) {
-  return (zodios: Zodios<Url, Api>) => {
+export function pluginApiKey<Api extends ZodiosEnpointDescriptions>(
+  provider: ApiKeyPluginConfig
+) {
+  return (zodios: Zodios<Api>) => {
     zodios.axios.interceptors.request.use(createRequestInterceptor(provider));
   };
 }
