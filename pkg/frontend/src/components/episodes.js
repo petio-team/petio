cc339982488bf40260d38196878b36605dc8a007
AshDyson <ashleyrobert@hotmail.co.uk>
2022-06-05 00:28:24 +0100

    fix(Episodes): Catch no episode data returned

--- pkg/frontend/src/components/episodes.js
+++ pkg/frontend/src/components/episodes.js
@@ -1,27 +1,26 @@
-import { useState } from "react";
-import { LazyLoadImage } from "react-lazy-load-image-component";
+import { useState } from 'react';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
-import { ReactComponent as ArrowIcon } from "../assets/svg/arrow.svg";
-import { ReactComponent as Tick } from "../assets/svg/check.svg";
-
-import styles from "../styles/views/tv.module.scss";
-import typo from "../styles/components/typography.module.scss";
+import { ReactComponent as ArrowIcon } from '../assets/svg/arrow.svg';
+import { ReactComponent as Tick } from '../assets/svg/check.svg';
+import typo from '../styles/components/typography.module.scss';
+import styles from '../styles/views/tv.module.scss';
 
 export default function Episodes({ data, mobile, globalRequests }) {
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [currentSeasonData, setCurrentSeasonData] = useState(
     data.seasonData[
       Object.keys(data.seasonData).filter(
-        (season) => data.seasonData[season].season_number === 1
+        (season) => data.seasonData[season].season_number === 1,
       )
-    ]
+    ],
   );
   const [expand, setExpand] = useState(false);
   const limit = mobile ? 1 : 2;
 
   function changeSeason(name) {
     const seasonRef = Object.keys(data.seasonData).filter(
-      (season) => data.seasonData[season].name === name
+      (season) => data.seasonData[season].name === name,
     );
     const seasonData = data.seasonData[seasonRef];
     setCurrentSeasonData(seasonData);
@@ -37,8 +36,8 @@ export default function Episodes({ data, mobile, globalRequests }) {
     let days = Math.round(
       Math.abs(
         (firstDate.setHours(0, 0, 0, 0) - secondDate.setHours(0, 0, 0, 0)) /
-          oneDay
-      )
+          oneDay,
+      ),
     );
 
     if (firstDate < secondDate) {
@@ -53,7 +52,7 @@ export default function Episodes({ data, mobile, globalRequests }) {
 
     return days < 100 ? (
       <span className={styles.episode__info__notAired}>
-        Airs in {days} {days > 1 ? "days" : "day"}
+        Airs in {days} {days > 1 ? 'days' : 'day'}
       </span>
     ) : (
       <span className={styles.episode__info__notAired}>Not aired</span>
@@ -61,6 +60,8 @@ export default function Episodes({ data, mobile, globalRequests }) {
   }
 
   const requested = globalRequests && globalRequests[data.id] ? true : false;
+
+  if (!currentSeasonData) return null;
 
   return (
     <div className={styles.episodes}>
@@ -71,11 +72,12 @@ export default function Episodes({ data, mobile, globalRequests }) {
               className={styles.episodes__title__dropdown}
               onClick={() => setDropdownOpen(dropdownOpen ? false : true)}
             >
-              {currentSeasonData.name} <ArrowIcon viewBox="0 0 12 20" />
-            </span>{" "}
-            <span className={typo.vertical_spacer}></span>{" "}
+              {currentSeasonData ? currentSeasonData.name : 'Unknown'}{' '}
+              <ArrowIcon viewBox="0 0 12 20" />
+            </span>{' '}
+            <span className={typo.vertical_spacer}></span>{' '}
             {currentSeasonData.episodes.length} Episode
-            {currentSeasonData.episodes.length !== 1 ? "s" : ""}
+            {currentSeasonData.episodes.length !== 1 ? 's' : ''}
           </p>
           {dropdownOpen ? (
             <div className={styles.episodes__all}>
@@ -85,7 +87,7 @@ export default function Episodes({ data, mobile, globalRequests }) {
                   if (season.name === currentSeasonData.name) {
                     style = {
                       opacity: 0.5,
-                      pointerEvents: "none",
+                      pointerEvents: 'none',
                     };
                   }
                   return (
@@ -187,7 +189,7 @@ export default function Episodes({ data, mobile, globalRequests }) {
                 {mobile ? null : (
                   <p className={`${typo.small} ${styles.episode__overview}`}>
                     {episode.overview.length > 160
-                      ? episode.overview.substring(0, 157) + "..."
+                      ? episode.overview.substring(0, 157) + '...'
                       : episode.overview}
                   </p>
                 )}
@@ -201,7 +203,7 @@ export default function Episodes({ data, mobile, globalRequests }) {
               className={`${typo.body} ${typo.uppercase} ${typo.bold}`}
               onClick={() => setExpand(expand ? false : true)}
             >
-              Show {expand ? "less" : "more"}
+              Show {expand ? 'less' : 'more'}
             </p>
           </div>
         ) : null}
