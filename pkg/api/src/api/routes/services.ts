1b66377f268327c3ab6b07cdf19b2ff8a5d13932
ADRFranklin <adrfranklin95@gmail.com>
2022-06-06 17:35:25 +0100

    fix(services): make subpath require a minimum length of 1 to pass validation

--- pkg/api/src/api/routes/services.ts
+++ pkg/api/src/api/routes/services.ts
@@ -43,7 +43,7 @@ const ArrInputSchema = z.array(
       }
       return parsed;
     }),
-    subpath: z.string().default('/'),
+    subpath: z.string().default('/').min(1),
     path: z.object({
       id: z.number(),
       location: z.string(),
