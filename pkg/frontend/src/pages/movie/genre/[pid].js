1be94f00b5a8b5a238a2ce3dc47690294474f364
AshDyson <ashleyrobert@hotmail.co.uk>
2022-06-06 21:07:34 +0100

    fix(Genres): Infinite Scroll

--- pkg/frontend/src/pages/movie/genre/[pid].js
+++ pkg/frontend/src/pages/movie/genre/[pid].js
@@ -1,45 +1,74 @@
-import media from "../../../services/media.service";
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-
-import hero from "../../../styles/components/hero.module.scss";
-import styles from "../../../styles/views/genre.module.scss";
-import typo from "../../../styles/components/typography.module.scss";
-
-import Meta from "../../../components/meta";
-import Hero from "../../../components/hero";
-import Grid from "../../../components/grid";
-import { matchGenre } from "../../../helpers/genres";
-import NotFound from "../../404";
+import NotFound from '../../404';
+import Grid from '../../../components/grid';
+import Hero from '../../../components/hero';
+import Meta from '../../../components/meta';
+import { matchGenre } from '../../../helpers/genres';
+import media from '../../../services/media.service';
+import hero from '../../../styles/components/hero.module.scss';
+import typo from '../../../styles/components/typography.module.scss';
+import styles from '../../../styles/views/genre.module.scss';
 
 export default function Genre({ newNotification }) {
-  const [genreName, setGenreName] = useState("");
+  const [genreName, setGenreName] = useState('');
   const [movies, setMovies] = useState(false);
-  // const [total, setTotal] = useState(1);
+  const [total, setTotal] = useState(1);
   const [featuredMovie, setFeaturedMovie] = useState(false);
   const { pid } = useParams();
+  const [loadingMore, setLoadingMore] = useState(false);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState(null);
 
   useEffect(() => {
     async function getGenreDetails() {
       try {
-        const genreMatch = matchGenre("movie", pid);
-        if (!genreMatch || !genreMatch.query) throw "not found";
+        const genreMatch = matchGenre('movie', pid);
+        if (!genreMatch || !genreMatch.query) throw 'not found';
         setGenreName(genreMatch.name);
-        const moviesLookup = await media.lookup("movie", 1, genreMatch.query);
+        setQuery(genreMatch.query);
+        const moviesLookup = await media.lookup('movie', 1, genreMatch.query);
         setMovies(moviesLookup.results);
-        // setTotal(moviesLookup.totalPages);
+        setTotal(moviesLookup.total_pages);
         setFeaturedMovie(moviesLookup.results[0]);
       } catch (e) {
         console.log(e);
-        setGenreName("error");
+        setGenreName('error');
       }
     }
 
     getGenreDetails();
   }, [pid]);
 
-  if (genreName === "error") return <NotFound />;
+  useEffect(() => {
+    async function loadMore() {
+      if (loadingMore || !query || page === total) return;
+      setLoadingMore(true);
+      const moviesLookup = await media.lookup('movie', page + 1, query);
+      setMovies([...movies, ...moviesLookup.results]);
+      setPage(page + 1);
+      setLoadingMore(false);
+    }
+
+    function handleScroll() {
+      if (
+        window.innerHeight * 2 + window.scrollY >=
+        document.body.offsetHeight
+      ) {
+        // you're at the bottom of the page
+        loadMore();
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [loadingMore, query, page, total, movies]);
+
+  if (genreName === 'error') return <NotFound />;
 
   return (
     <div className={styles.wrap} key={`genre_single_${pid}`}>
--- pkg/frontend/src/pages/tv/genre/[pid].js
+++ pkg/frontend/src/pages/tv/genre/[pid].js
@@ -1,45 +1,74 @@
-import media from "../../../services/media.service";
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-
-import hero from "../../../styles/components/hero.module.scss";
-import styles from "../../../styles/views/genre.module.scss";
-import typo from "../../../styles/components/typography.module.scss";
-
-import Meta from "../../../components/meta";
-import Hero from "../../../components/hero";
-import Grid from "../../../components/grid";
-import { matchGenre } from "../../../helpers/genres";
-import NotFound from "../../404";
+import NotFound from '../../404';
+import Grid from '../../../components/grid';
+import Hero from '../../../components/hero';
+import Meta from '../../../components/meta';
+import { matchGenre } from '../../../helpers/genres';
+import media from '../../../services/media.service';
+import hero from '../../../styles/components/hero.module.scss';
+import typo from '../../../styles/components/typography.module.scss';
+import styles from '../../../styles/views/genre.module.scss';
 
 export default function Genre({ newNotification }) {
-  const [genreName, setGenreName] = useState("");
+  const [genreName, setGenreName] = useState('');
   const [shows, setShows] = useState(false);
-  // const [total, setTotal] = useState(1);
+  const [total, setTotal] = useState(1);
   const [featuredShow, setfeaturedShow] = useState(false);
   const { pid } = useParams();
+  const [loadingMore, setLoadingMore] = useState(false);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState(null);
 
   useEffect(() => {
     async function getGenreDetails() {
       try {
-        const genreMatch = matchGenre("tv", pid);
-        if (!genreMatch || !genreMatch.query) throw "not found";
+        const genreMatch = matchGenre('tv', pid);
+        if (!genreMatch || !genreMatch.query) throw 'not found';
         setGenreName(genreMatch.name);
-        const showsLookup = await media.lookup("show", 1, genreMatch.query);
+        setQuery(genreMatch.query);
+        const showsLookup = await media.lookup('show', 1, genreMatch.query);
         setShows(showsLookup.results);
-        // setTotal(showsLookup.totalPages);
+        setTotal(showsLookup.totalPages);
         setfeaturedShow(showsLookup.results[0]);
       } catch (e) {
         console.log(e);
-        setGenreName("error");
+        setGenreName('error');
       }
     }
 
     getGenreDetails();
   }, [pid]);
 
-  if (genreName === "error") return <NotFound />;
+  useEffect(() => {
+    async function loadMore() {
+      if (loadingMore || !query || page === total) return;
+      setLoadingMore(true);
+      const showsLookup = await media.lookup('show', page + 1, query);
+      setShows([...shows, ...showsLookup.results]);
+      setPage(page + 1);
+      setLoadingMore(false);
+    }
+
+    function handleScroll() {
+      if (
+        window.innerHeight * 2 + window.scrollY >=
+        document.body.offsetHeight
+      ) {
+        // you're at the bottom of the page
+        loadMore();
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [loadingMore, query, page, total, shows]);
+
+  if (genreName === 'error') return <NotFound />;
 
   return (
     <div className={styles.wrap} key={`genre_single_show_${pid}`}>
