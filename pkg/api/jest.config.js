cb8344c688199efa918fcc7a0a8310b0298940ec
ADRFranklin <adrfranklin95@gmail.com>
2022-06-11 06:32:30 +0100

    feat: added jest for testing backend

---
+++ pkg/api/jest.config.js
@@ -0,0 +1,12 @@
+// jest.config.js
+const { pathsToModuleNameMapper } = require('ts-jest');
+// In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
+// which contains the path mapping (ie the `compilerOptions.paths` option):
+const { compilerOptions } = require('./tsconfig.json');
+
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+};
