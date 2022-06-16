40d784fc736e3bb3f978dbfdc4a4aabbc4f26099
ADRFranklin <adrfranklin95@gmail.com>
2022-06-16 11:34:04 +0100

    chore: catch app errors and handle unexpected errors

--- pkg/api/src/app.ts
+++ pkg/api/src/app.ts
@@ -5,5 +5,8 @@ import('dotenv/config');
 import('cache-manager/lib/stores/memory');
 
 (async () => {
-  (await import('./loaders')).default();
+  (await import('./loaders')).default().catch((error) => {
+    console.log('something unexpected went wrong');
+    console.log(error);
+  });
 })();
