d73d5d5296a2cb785886869079d2d673d48c56b3
ADRFranklin <adrfranklin95@gmail.com>
2022-08-30 00:21:57 +0100

    feat(api): startup message now includes the subpath

--- pkg/api/src/utils/startupMessage.ts
+++ pkg/api/src/utils/startupMessage.ts
@@ -5,12 +5,16 @@ import { HasConfig } from '@/config/config';
 import { config } from '@/config/schema';
 import logger from '@/loaders/logger';
 
+import { removeSlashes } from './urls';
+
 export default async () => {
+  const subpath = '/' + removeSlashes(config.get('petio.subpath'));
+
   logger.info(`Petio v${pkg.version} [${config.get('logger.level')}]`);
   logger.info(
-    `Listening on http://${config.get('petio.host')}:${config.get(
+    `Serving webui on http://${config.get('petio.host')}:${config.get(
       'petio.port',
-    )}`,
+    )}${subpath}`,
   );
   if (!(await HasConfig())) {
     logger.warn(
