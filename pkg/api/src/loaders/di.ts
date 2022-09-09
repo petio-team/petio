abff145b6d85f7ac796e4c3ca874f61b65cd6845
ADRFranklin <adrfranklin95@gmail.com>
2022-09-09 18:48:18 +0100

    fix(di): forgot to push changes to DI

--- pkg/api/src/loaders/di.ts
+++ pkg/api/src/loaders/di.ts
@@ -4,14 +4,26 @@ import { Logger } from 'winston';
 
 import logger from '@/loaders/logger';
 import MediaServerDB from '@/models/mediaserver/db';
-import { IMediaServerRepository } from '@/models/mediaserver/repository';
+import { IMediaServerRepository } from '@/models/mediaserver/repo';
+import { NotificationDB } from '@/models/notification/db';
+import { INotificationRepository } from '@/models/notification/repo';
+import { UserDB } from '@/models/user/db';
+import { IUserRepository } from '@/models/user/repo';
 
 export default () => {
   container.register<EventDispatcher>('Events', {
     useValue: new EventDispatcher(),
   });
   container.register<Logger>('Logger', { useValue: logger });
+
+  // Repositories
   container.register<IMediaServerRepository>('MediaServer', {
     useClass: MediaServerDB,
+  });
+  container.register<INotificationRepository>('Notification', {
+    useClass: NotificationDB,
+  });
+  container.register<IUserRepository>('User', {
+    useClass: UserDB,
   });
 };
