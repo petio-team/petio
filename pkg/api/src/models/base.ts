e764bcdcb01ed0e0e40228becc06c8463c10a082
ADRFranklin <adrfranklin95@gmail.com>
2022-09-09 16:21:00 +0100

    feat(resources): add common repository functions and schema base

---
+++ pkg/api/src/models/base.ts
@@ -0,0 +1,20 @@
+import { modelOptions, prop } from "@typegoose/typegoose";
+import { TimeStamps } from "@typegoose/typegoose/lib/defaultClasses";
+import { randomUUID as uuidv4 } from 'crypto';
+
+@modelOptions({ schemaOptions: { _id: false, versionKey: false } })
+export abstract class BaseSchema extends TimeStamps {
+  @prop({ default: uuidv4(), unique: true, immutable: true, index: true })
+  public id: string;
+}
+
+export interface IRead<T> {
+  GetAll(): Promise<T[]>;
+  GetById(id: string): Promise<T>;
+}
+
+export interface IWrite<T> {
+  Create(create: T): Promise<T>;
+  UpdateById(update: Partial<T>): Promise<T>;
+  DeleteById(id: string): Promise<void>;
+}
