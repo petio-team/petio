d0fa8d48f2a07d60b0b40f3db7dd1eb3086ac08e
ADRFranklin <adrfranklin95@gmail.com>
2022-09-09 17:32:21 +0100

    fix(resources): swap RemoveById and DeleteById around

--- pkg/api/src/models/request/repo.ts
+++ pkg/api/src/models/request/repo.ts
@@ -2,5 +2,5 @@ import { IRead, IWrite } from '../base';
 import { Request } from './dto';
 
 export interface IRequestRepository extends IWrite<Request>, IRead<Request> {
-  RemoveById(id: string): Promise<void>;
+  RemoveById(id: string): Promise<Request>;
 }
