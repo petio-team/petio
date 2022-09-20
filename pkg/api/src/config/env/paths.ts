d4c7e96478205db545da1b9c60be8bd52f9f820a
ADRFranklin <adrfranklin95@gmail.com>
2022-09-20 09:49:11 +0000

    fix(env): remove trailing slash from app dir

--- pkg/api/src/config/env/paths.ts
+++ pkg/api/src/config/env/paths.ts
@@ -11,7 +11,7 @@ const proc: Process = process;
 const ROOT_DIR = proc.pkg ? path.dirname(process.execPath) : proc.cwd();
 const APP_DIR: string = proc.pkg
   ? path.join(__dirname, '../../../../../')
-  : proc.env.APP_DIR ?? path.join(ROOT_DIR, '../../');
+  : proc.env.APP_DIR ?? path.join(ROOT_DIR, '../..');
 const VIEWS_DIR = process.env.VIEWS_FOLDER ?? path.join(APP_DIR, './pkg');
 const DATA_DIR = process.env.DATA_FOLDER ?? path.join(ROOT_DIR, './data');
 
