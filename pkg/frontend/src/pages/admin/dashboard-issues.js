77d6238770b9a0806aa737f357aac8108fe63f4d
AshDyson <ashleyrobert@hotmail.co.uk>
2022-09-21 17:43:22 +0100

    chore(admin): Add issues to dashboard

--- pkg/frontend/src/pages/admin/dashboard-issues.js
+++ pkg/frontend/src/pages/admin/dashboard-issues.js
@@ -1,5 +1,8 @@
+import { useEffect } from 'react';
 import { connect } from 'react-redux';
 
+import { getIssues } from '../../services/user.service';
+import typo from '../../styles/components/typography.module.scss';
 import styles from '../../styles/views/admin.module.scss';
 
 const mapStateToProps = (state) => {
@@ -7,15 +10,52 @@ const mapStateToProps = (state) => {
     requests: state.user.requests,
     redux_movies: state.media.movies,
     redux_tv: state.media.tv,
+    redux_issues: state.user.issues,
   };
 };
 
-function Issues() {
+function Issues({ redux_issues }) {
+  useEffect(() => {
+    getIssues();
+  });
+
+  if (redux_issues.length === 0)
+    return (
+      <div className={styles.dashboard__module}>
+        <p className={typo.body}>No issues reported</p>
+      </div>
+    );
+
   return (
     <div className={styles.dashboard__module}>
-      <p>Issues</p>
+      {redux_issues.map((issue) => {
+        return (
+          <div className={styles.dashboard__issue}>
+            <p className={typo.body}>
+              <b>{issue.title}:</b> {formatIssue(issue.issue)}
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
+}
+
+function formatIssue(issue) {
+  switch (issue) {
+    case 'season':
+      return 'Missing Season';
+    case 'subs':
+      return 'Missing Subtitles';
+    case 'bad-video':
+      return 'Bad Quality / Video Issue';
+    case 'bad-audio':
+      return 'Audio Issue / Audio Sync';
+    case 'other':
+      return 'Other';
+    default:
+      return '';
+  }
 }
 
 export default connect(mapStateToProps)(Issues);
