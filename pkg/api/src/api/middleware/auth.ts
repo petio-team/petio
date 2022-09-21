6f6d3307eb10d31a54b7779874a14e768d33c73e
ADRFranklin <adrfranklin95@gmail.com>
2022-09-21 03:33:37 +0000

    refactor(auth): remove cookie if it contains bad data

--- pkg/api/src/api/middleware/auth.ts
+++ pkg/api/src/api/middleware/auth.ts
@@ -6,25 +6,37 @@ import { config } from '@/config/schema';
 import logger from '@/loaders/logger';
 import { UserModel } from '@/models/user';
 
+function removeCookie(ctx: Context) {
+  logger.debug("removing invalid cookie")
+  ctx.cookies.set("petio-jwt", null, { expires: new Date(Date.now() - 2000) });
+}
+
 export async function authenticate(ctx: Context) {
-  const { authorization: header } = ctx.request.headers;
   const cookie = ctx.cookies.get('petio_jwt');
+  const bearerToken = ctx.request.header.authorization;
 
   let petioJwt;
   if (ctx.request.body.authToken) {
     petioJwt = ctx.request.body.authToken;
   } else if (cookie) {
     petioJwt = cookie;
-  } else if (header && /^Bearer (.*)$/.test(header)) {
-    const match: any = /^Bearer (.*)$/.exec(header);
-    petioJwt = match[1];
+  } else if (bearerToken) {
+    petioJwt = bearerToken
   } else {
     throw new Error(`AUTH: No auth token provided - route ${ctx.path}`);
   }
 
   let userData;
   try {
-    const jwtData = jwt.verify(petioJwt, config.get('plex.token'));
+    const jwtData: any = jwt.verify(petioJwt, config.get('plex.token'));
+    if (!jwtData) {
+      removeCookie(ctx);
+      throw new Error(`jwt data was invalid`);
+    }
+    if (!jwtData.id) {
+      removeCookie(ctx);
+      throw new Error(`id missing from jwt data`);
+    }
 
     const resp = await UserModel.findOne({ _id: (jwtData as any).id }).exec();
     if (!resp) {
