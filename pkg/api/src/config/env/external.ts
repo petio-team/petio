f7521bffae7d1832a9c0707397215b459f7e29e8
ADRFranklin <adrfranklin95@gmail.com>
2022-09-23 07:32:13 +0000

    chore: allow overriding external api keys via env

--- pkg/api/src/config/env/external.ts
+++ pkg/api/src/config/env/external.ts
@@ -4,8 +4,8 @@ type ExternalConfig = {
 };
 
 const externalConfig: ExternalConfig = {
-  tmdbApiKey: '1af5ad19a2d972a67cd27eb033979c4c',
-  fanartApiKey: 'ee409f6fb0c5cd2352e7a454d3f580d4',
+  tmdbApiKey: process.env.TMDB_API_KEY || '1af5ad19a2d972a67cd27eb033979c4c',
+  fanartApiKey: process.env.FANART_API_KEY || 'ee409f6fb0c5cd2352e7a454d3f580d4',
 };
 
 export default externalConfig;
