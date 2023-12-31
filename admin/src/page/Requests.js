8b885a411fbfb750cdd7e05b4cf840e2a4cd67df
JustMichael <adrfranklin95@gmail.com>
2023-12-31 23:05:43 +0000

    fix: renamed dataValue to data-value to conform to correct select properties

--- admin/src/page/Requests.js
+++ admin/src/page/Requests.js
@@ -350,11 +350,11 @@ class Requests extends React.Component {
                     ? this.state[`edit_${type}`][server.uuid]?.path
                     : false
                 }
-                // dataValue={
-                //   this.state[`edit_${type}`][server.uuid]?.path
-                //     ? this.state[`edit_${type}`][server.uuid]?.path
-                //     : false
-                // }
+                data-value={
+                  this.state[`edit_${type}`][server.uuid]?.path
+                    ? this.state[`edit_${type}`][server.uuid]?.path
+                    : false
+                }
                 onChange={this.changeServerSettings}
               >
                 <option value="">Please choose</option>
