4ae9a73af2df97a5ab7392e45309d3f3bfbadee0
JustMichael <adrfranklin95@gmail.com>
2024-04-03 17:22:23 +0000

    fix(http): http errors outputted data in reverse order

--- pkg/api/src/api/http/responseHandler.ts
+++ pkg/api/src/api/http/responseHandler.ts
@@ -2,47 +2,47 @@ import { StatusCodes } from "http-status-codes";
 import { DefaultContext } from "koa";
 
 export default () => async (ctx: DefaultContext, next: () => Promise<any>) => {
-  ctx.success = ({statusCode, data = undefined}: any) => {
-      const status = 'success';
+  ctx.success = ({ statusCode, data = undefined }: any) => {
+    const status = 'success';
 
-      if (!!statusCode && (statusCode < StatusCodes.BAD_REQUEST))
-          ctx.status = statusCode;
-      else if (!(ctx.status < StatusCodes.BAD_REQUEST))
-          ctx.status = StatusCodes.OK;
+    if (!!statusCode && (statusCode < StatusCodes.BAD_REQUEST))
+      ctx.status = statusCode;
+    else if (!(ctx.status < StatusCodes.BAD_REQUEST))
+      ctx.status = StatusCodes.OK;
 
-      ctx.body = {status, data};
+    ctx.body = { status, data };
   };
 
-  ctx.error = ({statusCode, code, message = undefined}: any) => {
-      const status = 'error';
+  ctx.error = ({ statusCode, code, message = undefined }: any) => {
+    const status = 'error';
 
-      if (!!statusCode && (statusCode >= StatusCodes.BAD_REQUEST && statusCode < 600))
-          ctx.status = statusCode;
-      else if (!(ctx.status >= StatusCodes.INTERNAL_SERVER_ERROR && ctx.status < 600))
-          ctx.status = StatusCodes.INTERNAL_SERVER_ERROR;
+    if (!!statusCode && (statusCode >= StatusCodes.BAD_REQUEST && statusCode < 600))
+      ctx.status = statusCode;
+    else if (!(ctx.status >= StatusCodes.INTERNAL_SERVER_ERROR && ctx.status < 600))
+      ctx.status = StatusCodes.INTERNAL_SERVER_ERROR;
 
-      ctx.body = {status, code, message};
+    ctx.body = { status, code, message };
   };
 
   ctx.ok = (params: any = {}) => {
-      ctx.success({
-          ...params,
-          statusCode: StatusCodes.OK,
-      });
+    ctx.success({
+      statusCode: StatusCodes.OK,
+      data: params,
+    });
   };
 
   ctx.created = (params: any = {}) => {
-      ctx.success({
-          ...params,
-          statusCode: StatusCodes.CREATED,
-      });
+    ctx.success({
+      statusCode: StatusCodes.CREATED,
+      data: params,
+    });
   };
 
   ctx.accepted = (params: any = {}) => {
-      ctx.success({
-          ...params,
-          statusCode: StatusCodes.ACCEPTED,
-      });
+    ctx.success({
+      statusCode: StatusCodes.ACCEPTED,
+      data: params,
+    });
   };
 
   await next();
