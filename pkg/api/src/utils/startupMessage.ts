347b0b9cde251a691c23f9ac3e1707e1df72f164
Michael <adrfranklin95@gmail.com>
2024-04-28 14:41:08 +0000

    fix(startup): welcome message was using wrong variable for log level

--- pkg/api/src/utils/startupMessage.ts
+++ pkg/api/src/utils/startupMessage.ts
@@ -1,11 +1,11 @@
-import { removeSlashes } from './urls';
 import appConfig from "@/config/env/app";
 import config from '@/config/schema';
 import logger from '@/loaders/logger';
+import { removeSlashes } from './urls';
 
 export default () => {
   const subpath = `/${  removeSlashes(config.get('petio.subpath'))}`;
-  logger.info(`Petio v${appConfig.version} [${config.get('logger.level')}]`);
+  logger.info(`Petio v${appConfig.version} [${logger.core().level}]`);
   logger.info(
     `Serving Web UI on http://${config.get('petio.host')}:${config.get(
       'petio.port',
