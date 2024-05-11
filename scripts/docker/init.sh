dbb2069c85445af824ad447e3784719e52dd1a91
Michael <adrfranklin95@gmail.com>
2024-05-11 16:23:35 +0000

    chore: remove old docker script folder

--- scripts/docker/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [[ ! ${PUID} -eq 0 ]] && [[ ! ${PGID} -eq 0 ]]; then
-	usermod -o -u "${PUID}" petio
-	groupmod -o -g "${PGID}" petio
-	usermod -d "/data" petio
-fi
-chmod "=rwx" "/data"
-chown petio:petio "/data"
-su petio -c "node --no-deprecation ./index.js --host 0.0.0.0 --port 7777"
