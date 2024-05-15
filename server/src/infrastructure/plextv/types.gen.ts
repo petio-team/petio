c0b9ea65aa306d3566b14279903d6bf352ccd2c5
Michael <adrfranklin95@gmail.com>
2024-05-15 19:13:31 +0000

    refactor(server): added missing types for user generated by openapi

--- server/src/infrastructure/plextv/types.gen.ts
+++ server/src/infrastructure/plextv/types.gen.ts
@@ -132,7 +132,67 @@ export type GetDevicesResponse = Array<{
   }>;
 }>;
 
-export type GetUserDetailsResponse = unknown;
+export type GetUserDetailsResponse = {
+  id: number;
+  uuid: string;
+  username: string;
+  title: string;
+  email: string;
+  friendlyName: string;
+  locale: any;
+  confirmed: boolean;
+  joinedAt: number;
+  emailOnlyAuth: boolean;
+  hasPassword: boolean;
+  protected: boolean;
+  thumb: string;
+  authToken: string;
+  mailingListStatus: string;
+  mailingListActive: boolean;
+  scrobbleTypes: string;
+  country: string;
+  subscription: {
+    active: boolean;
+    subscribedAt: string;
+    status: string;
+    paymentService: string;
+    plan: string;
+    features: Array<string>;
+  };
+  subscriptionDescription: string;
+  restricted: boolean;
+  anonymous: boolean;
+  home: boolean;
+  guest: boolean;
+  homeSize: number;
+  homeAdmin: boolean;
+  maxHomeSize: number;
+  rememberExpiresAt: number;
+  profile: {
+    autoSelectAudio: boolean;
+    defaultAudioLanguage: string;
+    defaultSubtitleLanguage: string;
+    autoSelectSubtitle: number;
+    defaultSubtitleAccessibility: number;
+    defaultSubtitleForced: number;
+    watchedIndicator: number;
+  };
+  entitlements: Array<string>;
+  roles: Array<string>;
+  services: Array<{
+    identifier: string;
+    endpoint: string;
+    token?: string;
+    secret?: string;
+    status: string;
+  }>;
+  adsConsent: any;
+  adsConsentSetAt: any;
+  adsConsentReminderAt: any;
+  experimentalFeatures: boolean;
+  twoFactorEnabled: boolean;
+  backupCodesCreated: boolean;
+};
 
 export type GetUserSettingsResponse = Array<{
   id?: string;
