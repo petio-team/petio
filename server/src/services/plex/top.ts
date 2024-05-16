ce2f998df091ead04f28c743037ce891f5393d30
Michael <adrfranklin95@gmail.com>
2024-05-16 17:13:28 +0000

    refactor(server): moved plex top request to top module

--- server/src/services/plex/top.ts
+++ server/src/services/plex/top.ts
@@ -1,10 +1,7 @@
 /* eslint-disable @typescript-eslint/no-use-before-define */
 import { getFromContainer } from '@/infrastructure/container/container';
 import loggerMain from '@/infrastructure/logger/logger';
-import {
-  GetLibraryTopContentResponse,
-  getTopWatchedMedia,
-} from '@/infrastructure/plex/custom/topWatchedMedia';
+import { CancelablePromise, PlexClient } from '@/infrastructure/plex';
 import is from '@/infrastructure/utils/is';
 import { MediaServerEntity } from '@/resources/media-server/entity';
 import { CacheService } from '@/services/cache/cache';
@@ -14,6 +11,91 @@ import { movieLookup } from '@/services/tmdb/movie';
 import { showLookup } from '@/services/tmdb/show';
 
 const logger = loggerMain.child({ module: 'plex.top' });
+
+export type GetLibraryTopContentResponse = {
+  MediaContainer?: {
+    size?: number;
+    allowSync?: boolean;
+    identifier?: string;
+    mediaTagPrefix?: string;
+    mediaTagVersion?: number;
+    Metadata?: Array<{
+      ratingKey?: string;
+      key?: string;
+      guid?: string;
+      slug?: string;
+      studio?: string;
+      type?: string;
+      title?: string;
+      librarySectionTitle?: string;
+      librarySectionID?: number;
+      librarySectionKey?: string;
+      contentRating?: string;
+      summary?: string;
+      index?: number;
+      audienceRating?: number;
+      year?: number;
+      thumb?: string;
+      art?: string;
+      theme?: string;
+      duration?: number;
+      originallyAvailableAt?: string;
+      leafCount?: number;
+      viewedLeafCount?: number;
+      childCount?: number;
+      addedAt?: number;
+      updatedAt?: number;
+      globalViewCount?: number;
+      userCount?: number;
+      audienceRatingImage?: string;
+      Genre?: Array<{
+        tag?: string;
+      }>;
+      Country?: Array<{
+        tag?: string;
+      }>;
+      Role?: Array<{
+        tag?: string;
+      }>;
+      User?: Array<{
+        id?: number;
+      }>;
+      originalTitle?: string;
+      titleSort?: string;
+      primaryExtraKey?: string;
+      tagline?: string;
+      seasonCount?: number;
+    }>;
+  };
+};
+
+export type GetLibraryTopContentParams = {
+  type: number;
+  accountId?: string;
+  'viewedAt>'?: number;
+  limit?: number;
+};
+
+/**
+ * Get Top Watched Content From Libraries
+ */
+export const getTopWatched = (
+  client: PlexClient,
+  data: GetLibraryTopContentParams,
+): CancelablePromise<GetLibraryTopContentResponse> =>
+  client.request.request({
+    method: 'GET',
+    url: '/library/all/top',
+    query: {
+      type: data.type,
+      'viewedAt>': data['viewedAt>'],
+      limit: data.limit,
+    },
+    errors: {
+      400: 'Bad Request - A parameter was not specified, or was specified incorrectly.',
+      401: 'Unauthorized - Returned if the X-Plex-Token is missing from the header or query.',
+    },
+  });
 
 export default async (server: MediaServerEntity, type: 1 | 2) => {
   try {
@@ -37,7 +119,7 @@ async function getTopData(server: MediaServerEntity, type: 1 | 2) {
   const timestamp = d.getTime() / 1000 || 0;
 
   try {
-    const data = await getTopWatchedMedia(client, {
+    const data = await getTopWatched(client, {
       type,
       limit: 20,
       'viewedAt>': timestamp,
