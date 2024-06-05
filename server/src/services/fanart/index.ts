4dc859c8e6b26ab5e70e1923202e08322f0c2516
Michael <adrfranklin95@gmail.com>
2024-06-05 11:00:49 +0000

    refactor(server): updated legacy fanart client

--- server/src/services/fanart/index.ts
+++ server/src/services/fanart/index.ts
@@ -1,5 +1,5 @@
 import { getFromContainer } from '@/infrastructure/container/container';
-import { FanartAPI } from '@/infrastructure/fanart/api';
+import { FanartTVAPI } from '@/infrastructure/fanart/client';
 import loggerMain from '@/infrastructure/logger/logger';
 import { CacheService } from '@/services/cache/cache';
 
@@ -8,14 +8,20 @@ const logger = loggerMain.child({ module: 'fanart.index' });
 export default async (id: string, type: any) => {
   let data: any = {};
   try {
-    data = getFromContainer(CacheService).wrap(id, async () =>
-      FanartAPI.get('/:type/:id', {
-        params: {
-          id,
-          type,
-        },
-      }),
-    );
+    data = getFromContainer(CacheService).wrap(id, async () => {
+      const client = getFromContainer(FanartTVAPI);
+      switch (type) {
+        case 'movie': {
+          return client.movie.getMovieImages({ movieId: id });
+        }
+        case 'tv': {
+          return client.tv.getTvImages({ showId: id });
+        }
+        default: {
+          throw new Error(`invalid fanart type ${type} for ${id}`);
+        }
+      }
+    });
   } catch (err) {
     logger.error(`failed to get fanart for ${id} (${type})`, err);
   }
