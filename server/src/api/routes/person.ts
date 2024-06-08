af6dfda0777d168678d5cf7cd32ef4270d95ee40
Michael <adrfranklin95@gmail.com>
2024-06-08 11:16:41 +0000

    refactor(server): updated routes to make use of new services

--- server/src/api/routes/person.ts
+++ server/src/api/routes/person.ts
@@ -2,14 +2,48 @@ import Router from '@koa/router';
 import { StatusCodes } from 'http-status-codes';
 import { Context } from 'koa';
 
-import personLookup from '@/services/tmdb/person';
+import { getFromContainer } from '@/infrastructure/container/container';
+import { PersonMapper } from '@/resources/person/mapper';
+import { PersonService } from '@/services/person/person-service';
 
 const lookupById = async (ctx: Context) => {
-  const data = await personLookup(ctx.params.id);
-
+  const mapper = getFromContainer(PersonMapper);
+  const results = await getFromContainer(PersonService).getDetails(
+    ctx.params.id,
+  );
+  if (results.isNone()) {
+    ctx.status = StatusCodes.NOT_FOUND;
+    return;
+  }
+  const result = results.unwrap();
   ctx.status = StatusCodes.OK;
-  ctx.body = data;
+  ctx.body = {
+    info: mapper.toResponse(result),
+    movies: {
+      id: ctx.params.id,
+      cast: result.media.movies.map((movie) => ({
+        id: movie.provider.tmdbId,
+        name: movie.name,
+      })),
+      crew: [],
+    },
+    tv: {
+      id: ctx.params.id,
+      cast: result.media.shows.map((show) => ({
+        id: show.provider.tmdbId,
+        name: show.name,
+      })),
+      crew: [],
+    },
+  };
 };
+
+// const lookupById = async (ctx: Context) => {
+//   const data = await personLookup(ctx.params.id);
+
+//   ctx.status = StatusCodes.OK;
+//   ctx.body = data;
+// };
 
 const route = new Router({ prefix: '/person' });
 
--- server/src/api/routes/trending.ts
+++ server/src/api/routes/trending.ts
@@ -2,11 +2,35 @@ import Router from '@koa/router';
 import { StatusCodes } from 'http-status-codes';
 import { Context } from 'koa';
 
-import trending from '@/services/tmdb/trending';
+import { getFromContainer } from '@/infrastructure/container/container';
+import { CompanyMapper } from '@/resources/company/mapper';
+import { MovieMapper } from '@/resources/movie/mapper';
+import { NetworkMapper } from '@/resources/network/mapper';
+import { PersonMapper } from '@/resources/person/mapper';
+import { ShowMapper } from '@/resources/show/mapper';
+import { CacheService } from '@/services/cache/cache-service';
 
 const getTrending = async (ctx: Context) => {
+  const service = getFromContainer(CacheService);
+  const movieMapper = getFromContainer(MovieMapper);
+  const showMapper = getFromContainer(ShowMapper);
+  const personMapper = getFromContainer(PersonMapper);
+  const networkMapper = getFromContainer(NetworkMapper);
+  const companyMapper = getFromContainer(CompanyMapper);
+  const trending = await service.getCommonResources();
+
   ctx.status = StatusCodes.OK;
-  ctx.body = await trending();
+  ctx.body = {
+    movies: trending.movies.map((movie) => movieMapper.toResponse(movie)),
+    tv: trending.shows.map((show) => showMapper.toResponse(show)),
+    people: trending.people.map((person) => personMapper.toResponse(person)),
+    networks: trending.networks.map((network) =>
+      networkMapper.toResponse(network),
+    ),
+    companies: trending.companies.map((company) =>
+      companyMapper.toResponse(company),
+    ),
+  };
 };
 
 const route = new Router({ prefix: '/trending' });
